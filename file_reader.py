22d09e5c5f8f5c7ecc7ec4472bafa419137368da
Gocze Norbert <gnorbi951@gmail.com>
2020-12-29 11:58:04 +0100

    Debug file_reader, init main.py

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,4 @@
 def read_file_to_list(filename):
     file = open(filename, "r")
     input_list = [int(line.split('\n')[0]) for line in file]
-    print(input_list)
+    return input_list
---
+++ main.py
@@ -0,0 +1,9 @@
+from file_reader import read_file_to_list 
+
+def solve_dojo():
+    input_list = read_file_to_list("input.txt")
+    print(input_list)
+
+
+if __name__ == "__main__":
+    solve_dojo()
